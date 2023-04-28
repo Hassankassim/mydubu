33cab0e641ec242bd4cf1f81bf7682d222188bda
HASSAN KASSIM ELIAS <114056773+Hassankassim@users.noreply.github.com>
2023-04-28 16:16:17 +0300

    the react part of the dubu
    first upload via git desktop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
+import './styles.css';
+import dubulogo from './img/dubu icon.png'
 
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand"><span><img width="52px" src={dubulogo} alt="" /></span></a>
+
+          <a id="h5" href=""><h5>Explore</h5></a>
+
+        </div>
+      </nav>
+
+      {/* Main content */}
+      <div className="container mt-5">
+        <div className="title">
+          <h3>TheBucket</h3>
+        </div>
+        <div className="row">
+          <div className="col-lg-6 col-sm-6">
+            <div className="form-group has-default bmd-form-group">
+              <input type="text" className="form-control" placeholder="Your Name" />
+            </div>
+          </div>
+
+          <div className="col-lg-6 col-sm-6">
+            <div className="form-group bmd-form-group">
+              <label htmlFor="exampleInput1" className="bmd-label-floating">Phone Number</label>
+              <input type="email" className="form-control" id="exampleInput1" />
+              <span className="bmd-help">+255.....</span>
+            </div>
+            <div className="button1">
+              <button type="submit" className="neu-button">Save</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <div className="futa" style={{ height: '430px' }}></div>
+
+    </>
   );
 }
 
